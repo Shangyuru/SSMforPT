369cea6ad9e5a206571d3b1816e820804741f24c
Shangyuru <943245719@qq.com>
2023-06-26 10:47:20 +0800

    2023/6/26

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @author syr
+ * @Date Created in ${YEAR} ${MONTH} ${DATE} ${TIME}
+ * @Discription
+ * @Package org.example
+ * @ProjectName ${PROJECT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
